facb89f700eb927089f24c4aca833f82f25fe235
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-29 10:57:44 +0530

    Initial Commit

---
+++ AnimalsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalsList
+//
+//  Created by Venkata Bhargav on 29/06/20.
+//  Copyright © 2020 venkat bhargav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
